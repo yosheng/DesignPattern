2a2240fd19e059d381ed57d686b480b20d94cc14
yosheng <sad445566@gmail.com>
2024-05-14 19:32:43 +0800

    feat: TemplateMethodPattern

---
+++ src/BehaviorPattern/TemplateMethodPattern/User.cs
@@ -0,0 +1,19 @@
+﻿namespace BehaviorPattern.TemplateMethodPattern;
+
+public abstract class User
+{
+    protected readonly string Name;
+
+    protected User(string name)
+    {
+        Name = name;
+    }
+    
+    protected abstract void CheckWork();
+    
+    public void GoOff()
+    {
+        CheckWork();
+        Console.WriteLine($"{Name}下班了");
+    }
+}
---
+++ test/BehaviorPattern.Tests/TemplateMethodPattern/TemplateMethodPatternTest.cs
@@ -0,0 +1,21 @@
+﻿using BehaviorPattern.TemplateMethodPattern;
+using Xunit.Abstractions;
+
+namespace BehaviorPattern.Tests.TemplateMethodPattern;
+
+public class TemplateMethodPatternTest : XunitContextBase
+{
+    public TemplateMethodPatternTest(ITestOutputHelper output) : base(output)
+    {
+    }
+    
+    [Fact]
+    public void Test()
+    {
+        var doctor = new Doctor("张三");
+        doctor.GoOff();
+        
+        var nurse = new Nurse("小花");
+        nurse.GoOff();
+    }
+}
