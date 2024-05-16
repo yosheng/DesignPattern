dc19a0ed247eee3734fcea2ad6f63592693710d8
yosheng <sad445566@gmail.com>
2024-05-16 10:40:08 +0800

    feat: MementoPattern

---
+++ src/BehaviorPattern/MementoPattern/MedicalRecord.cs
@@ -0,0 +1,22 @@
+﻿namespace BehaviorPattern.MementoPattern;
+
+/// <summary>
+/// Originator 发起者
+/// </summary>
+public class MedicalRecord
+{
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public MedicalRecordMemo Save()
+    {
+        return new MedicalRecordMemo(Title, Content);
+    }
+
+    public void Restore(MedicalRecordMemo memo)
+    {
+        Title = memo.Title;
+        Content = memo.Content;
+    }
+}
---
+++ test/BehaviorPattern.Tests/MementoPattern/MementoPatternTest.cs
@@ -0,0 +1,32 @@
+﻿using BehaviorPattern.MementoPattern;
+using Xunit.Abstractions;
+
+namespace BehaviorPattern.Tests.MementoPattern;
+
+public class MementoPatternTest : XunitContextBase
+{
+    public MementoPatternTest(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var medicalRecord = new MedicalRecord
+        {
+            Title = "张三",
+            Content = "张三的病历"
+        };
+        
+        var medicalRecordCaretaker = new MedicalRecordCaretaker();
+        medicalRecordCaretaker.Memo = medicalRecord.Save();
+        
+        medicalRecord.Title = "李四";
+        medicalRecord.Content = "李四的病历";
+        
+        medicalRecord.Restore(medicalRecordCaretaker.Memo);
+        
+        Output.WriteLine($"Title: {medicalRecord.Title}");
+        Output.WriteLine($"Content: {medicalRecord.Content}");
+    }
+}
